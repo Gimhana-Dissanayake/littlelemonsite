347cc351f862e0ce0fa58f2a51102324c48b980e
GimhanaDissanayake <gimhanadev@gmail.com>
2024-01-03 08:04:49 +0530

    Created components Header, Navigation, Main, and Footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { Fragment } from "react";
 import "./App.css";
+import Navigation from "./components/Navigation";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 function App() {
-  return <div className="App">HomePage</div>;
+  return (
+    <Fragment>
+      <Header />
+      <Navigation />
+      <Main />
+      <Footer />
+    </Fragment>
+  );
 }
 
 export default App;
